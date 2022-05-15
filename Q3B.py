6405eb8c370ed193d58b27091ea169002edab582
Omer1 <omer.hadar96@gmail.com>
2022-05-15 17:09:50 +0300

    q1, q2, q3 solved

--- Q3B.py
+++ Q3B.py
@@ -4,4 +4,66 @@ def question3_b(mat):
     """
     # WRITE YOUR CODE HERE!
 
-    def
+    def size_of_group(mat1, row, col):
+        """
+
+        :param mat1:
+        :param row:
+        :param col:
+        :param num:
+        :return:
+        """
+        if mat1[row][col] == 0:
+            if col < len(mat1[0])-1 and mat1[row][col + 1] == 0:
+                mat3 = mat1[:]
+                mat3[row][col] = 1
+                return 1 + size_of_group(mat3, row, col + 1,)
+            elif col > 0 and mat1[row][col - 1] == 0:
+                mat3 = mat1[:]
+                mat3[row][col] = 1
+                return 1 + size_of_group(mat3, row, col - 1,)
+            elif row < len(mat1)-1 and mat1[row + 1][col] == 0:
+                mat3 = mat1[:]
+                mat3[row][col] = 1
+                return 1 + size_of_group(mat3, row + 1, col,)
+            elif row > 1 and mat1[row - 1][col] == 0:
+                mat3 = mat1[:]
+                mat3[row][col] = 1
+                return 1 + size_of_group(mat3, - 1, col,)
+        else:
+            return 0
+
+    def find_max_group(mat2, row2, col2, count=0):
+        if row2 == len(mat2):
+            return count
+        if col2 == len(mat2[0]):
+            if size_of_group(mat2, row2, col2) > count:
+                count = size_of_group(mat2, row2, col2)
+                return find_max_group(mat2, row2+1, 0, count)
+        else:
+            if size_of_group(mat2, row2, col2) > count:
+                count = size_of_group(mat2, row2, col2)
+                return find_max_group(mat2, row2, col2+1)
+
+    return find_max_group(mat, 0, 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
